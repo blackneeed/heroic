2b6363bdec740d60611c3ba1b212cb74d50bc89f
unknown <121721929+blackneeed@users.noreply.github.com>
2026-01-03 13:12:15 +0100

    Hello World

--- src/stage1.asm
+++ src/stage1.asm
@@ -7,7 +7,23 @@ main:
 
     mov sp, 0x7c00
 
+    mov si, hello_string
+    call print_string
+
     jmp $
+
+print_string:
+    mov ah, 0x0e
+    .loop:
+    lodsb
+    test al, al
+    jz .end
+    int 0x10
+    jmp .loop
+    .end:
+    ret
+
+hello_string: db "Hello World!", 0x0A, 0x0D, 0
 
 times 510-($-$$) db 0
 db 0x55, 0xaa
